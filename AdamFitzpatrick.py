dd0052eb58ef09809fa6dcc08256c2b940bb5b57
AdamFitz03 <adamfitz03@gmail.com>
2025-04-05 19:12:01 -0700

    Updated Adam.py

--- AdamFitzpatrick.py
+++ AdamFitzpatrick.py
@@ -1,12 +1,19 @@
 #Function 1
 # Returns Area of Rectangle
 def rect_area(l,w):
-    """ Multiplies, length (l) time width (w) to find the area of the rectangle
+    """ Function that returns the result of multiplying, length (l) time width (w) to find the area of the rectangle
         based of user inputted dimensions. written by Adam Fitzpatrick. """
     return l * w
 #Function 2
 # Returns Surface Area of Rectangular Solid
+def rect_surface_area (l,w,h):
+    """Calculates the surface area of a rectangular solid given the length(l), width(w), and height(h). Written by Brendan Flynn"""
+    # The surface area of a rectangular solid is given by 2*(l*w + l*h + w*h)
+    area_1 = rect_area(l,w)
+    area_2 = rect_area(l,h)
+    area_3 = rect_area(w,h)
 
+    return 2 * (area_1 + area_2 + area_3)
 
 # Request the dimension of a solid rectangular object
 
