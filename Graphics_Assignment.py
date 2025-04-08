f01913cbb7f77afc73f5afcf6577000e90e1465e
AdamFitz03 <adamfitz03@gmail.com>
2025-04-07 19:04:46 -0700

    Made more changes

--- Graphics_Assignment.py
+++ Graphics_Assignment.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plot
 # set up your lists
-numlist = [10, 6, 5, 3]
+numlist = [10, 3, 4, 3]
 namelist = ['Adam', 'Not Adam', 'James', 'Also Not Adam']
 colorlist = ['lightsalmon', 'tomato', 'peru', 'darkkhaki' ]
-explodelist = [0.04, 0.03, 0.02, 0.01]
+explodelist = [0.04, 0.02, 0.02, 0.01]
 # make the pie chart
 plot.pie(numlist, labels=namelist, autopct='%.1f%%', colors=colorlist,
-    	explode = explodelist, startangle = 60)
+    	explode = explodelist, startangle = 45)
 plot.axis('equal')
 plot.savefig('piechart.png')
